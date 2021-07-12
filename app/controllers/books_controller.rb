8143b3de068333a985b3ee049a16d1dbc869efd9
nijimajohn <hirokikotera0211@gmail.com>
2021-07-12 10:24:00 +0000

    [mid]3-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,15 +5,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book),notice: "Book was successfully created"
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -21,10 +26,21 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book),notice: "Book was successfully updated"
+    else
+      @books = Book.all
+      render action: :edit 
+    end
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path,notice: "Book was succesfully destroyed"
+  end
+    
     
   
   private
